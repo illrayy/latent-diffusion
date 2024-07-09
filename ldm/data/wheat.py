fe9054a741265dc11fc9ddce1c0a29aa88ac7e08
illrayy <93829503+illrayy@users.noreply.github.com>
2024-07-09 14:35:33 +0900

    Update wheat.py

--- ldm/data/wheat.py
+++ ldm/data/wheat.py
@@ -1,9 +1,11 @@
 import os
 import numpy as np
+import cv2
 import PIL
-from PIL import Image
+import random
 from torch.utils.data import Dataset
 from torchvision import transforms
+from transformers import AutoImageProcessor
 
 
 class wheatBase(Dataset):
@@ -11,9 +13,12 @@ class wheatBase(Dataset):
                  txt_file,
                  data_root,
                  size=None,
+                 p_dorpout=0,
                  interpolation="bicubic",
                  flip_p=0.5,
-                 degradation=None
+                 ag_rate=0.8,
+                 degradation=None,
+                 crop_size=(256,256)
                  ):
         self.data_paths = txt_file
         self.data_root = data_root
@@ -32,40 +37,114 @@ class wheatBase(Dataset):
                               "bicubic": PIL.Image.BICUBIC,
                               "lanczos": PIL.Image.LANCZOS,
                               }[interpolation]
-        self.flip = transforms.RandomHorizontalFlip(p=flip_p)
+        self.flip = flip_p
+        self.ag_rate = ag_rate
+        self.image_processor = AutoImageProcessor.from_pretrained("facebook/vit-mae-base")
+        self.crop_size = crop_size
+        self.p_dorpout = p_dorpout
+        
 
     def __len__(self):
         return self._length
 
     def __getitem__(self, i):
         example = dict((k, self.labels[k][i]) for k in self.labels)
-        image = Image.open(example["file_path_"])
-        if not image.mode == "RGB":
-            image = image.convert("RGB")
 
-        # default to score-sde preprocessing
-        img = np.array(image).astype(np.uint8)
-        crop = min(img.shape[0], img.shape[1])
-        h, w, = img.shape[0], img.shape[1]
-        img = img[(h - crop) // 2:(h + crop) // 2,
-              (w - crop) // 2:(w + crop) // 2]
+        image = cv2.imread(example["file_path_"])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_cp = image.copy()
+        h, w, c = image.shape
+        
 
-        image = Image.fromarray(img)
-        if self.size is not None:
-            image = image.resize((self.size, self.size), resample=self.interpolation)
+        y_ref_start = np.random.randint(0, h - self.crop_size[0] + 1)
+        x_ref_start = np.random.randint(0, w - self.crop_size[1] + 1)
+        reference = image[y_ref_start:y_ref_start + self.crop_size[0], x_ref_start:x_ref_start + self.crop_size[1]]
+        reference = self.image_processor(images=reference)['pixel_values'][0]
 
-        image = self.flip(image)
+        
+        if random.random() < self.p_dorpout:
+            reference = np.zeros_like(reference)
+                                                                                                
+        
+        if random.random()<self.ag_rate:
+            #创建正方形的四个顶点坐标和中心坐标
+            square_size = 512
+            center = (256, 256)
+            square_points = np.array([(0, 0), (0, square_size), (square_size, 0), (square_size, square_size)], dtype=np.float32)
+
+            #随机生成旋转角度
+            angle = random.uniform(-90, 90)
+
+            #创建顶点旋转矩阵并应用旋转
+            rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+            rotated_points = cv2.transform(square_points.reshape(1, -1, 2), rotation_matrix)
+
+            y_min = int(np.min(rotated_points[0, :, 1]))
+            y_max = int(np.max(rotated_points[0, :, 1]))
+            max_scale = square_size/(y_max - y_min)
+
+            #将缩放因子设置为（0.5，max_scale)
+            scale_factor = random.uniform(0.6, max_scale)
+
+            # 创建缩放矩阵
+            scaling_matrix = np.array([[scale_factor, 0, (1 - scale_factor) * center[0]],
+                                    [0, scale_factor, (1 - scale_factor) * center[1]]], dtype=np.float32)
+
+            # 应用缩放
+            scaled_points = cv2.transform(rotated_points, scaling_matrix)
+
+            # 将坐标四舍五入为整数
+            scaled_points = np.int32(scaled_points)
+
+            transformed_x_min = int(np.min(scaled_points[:, :, 0]))
+            transformed_y_min = int(np.min(scaled_points[:, :, 1]))
+
+            #随机生成平移量，并平移
+            shift_x = random.randint(-transformed_x_min, transformed_x_min)
+            shift_y = random.randint(-transformed_y_min, transformed_y_min)
+
+            scaled_points[:, :, 0] += shift_x
+            scaled_points[:, :, 1] += shift_y
+
+
+            back_rotation_matrix = cv2.getRotationMatrix2D(center, -angle, 1.0)
+
+            # 执行旋转
+            rotated_image = cv2.warpAffine(image, back_rotation_matrix, (h, w))
+            scaled_points = cv2.transform(scaled_points.reshape(1, -1, 2), back_rotation_matrix)
+            scaled_points = np.int32(scaled_points)
+
+            x_max = np.max(scaled_points[:, :, 0])
+            x_min = np.min(scaled_points[:, :, 0])
+            y_max = np.max(scaled_points[:, :, 1])
+            y_min = np.min(scaled_points[:, :, 1])
+
+            image = rotated_image[y_min:y_max, x_min:x_max]
+        
+        try:
+            image = cv2.resize(image, (self.size, self.size))
+        except:
+            image = cv2.resize(image_cp, (self.size, self.size))
+            print("file_path_")
+            print(x_max, x_min, y_max, y_min)
+            
+
+        if random.random()<self.flip:
+            image = cv2.flip(image, 0)
         image = np.array(image).astype(np.uint8)
+
         example["image"] = (image / 127.5 - 1.0).astype(np.float32)
+        example["reference"] = reference
         return example
 
 
 class wheatTrain(wheatBase):
-    def __init__(self, **kwargs):
-        super().__init__(txt_file="F:/wheat/256/train.txt", data_root="F:/wheat/256/train", **kwargs)
+    def __init__(self, ag_rate=0.8, **kwargs):
+        super().__init__(data_root="wheat/target1",
+                         ag_rate=ag_rate,**kwargs)
 
 
 class wheatValidation(wheatBase):
-    def __init__(self, flip_p=0., **kwargs):
-        super().__init__(txt_file="F:/wheat/256/val.txt", data_root="F:/wheat/256/val",
-                         flip_p=flip_p, **kwargs)
+    def __init__(self, flip_p=0.,ag_rate=0., **kwargs):
+        super().__init__(txt_file="wheat/val_cldm.txt", data_root="wheat/target1",
+                         flip_p=flip_p, ag_rate=ag_rate, **kwargs)
